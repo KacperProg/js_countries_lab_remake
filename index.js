5f9e970b6b49abcb75fbd6754e85dcfbd5ed7150
Kacper <kapelek82@outlook.com>
2023-12-03 23:54:11 +0000

    found working for loop to acess languages

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const getCountryByName = async (countryName)  => {
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
     const jsonData = await response.json();
-    console.log(jsonData[0].name.common);
-    document.getElementById("countryInfo").innerText = `Country Name: ${jsonData[0].name.common}`;
-    // document.getElementById("countryInfo").innerText = `Capital: ${jsonData[0].capital}`;
-    // document.getElementById("countryInfo").innerText = `Languages: ${jsonData[0].languages}`;
-    // document.getElementById("countryInfo").innerText = `Population: ${jsonData[0].population}`;
+    const name = jsonData[0].name.common;
+    // document.querySelector("body").appendChild(name);
+    const capital = jsonData[0].capital[0];
+    // document.querySelector("body").appendChild(capital);
+    const languages = jsonData[0].languages;
+    // languages.array.forEach(element => {
+        
+    for (const key in languages) {
+        
+        console.log(`Found the value '${languages[key]}' at key '${key}'`);
+    }
+    // });
+    const population = jsonData[0].population;
+    // document.querySelector("body").appendChild(population);
+
+    console.log(languages);
+
+    
+
+    document.getElementById("countryInfo").innerText = `Country Name: ${name}\nCapital: ${capital}\nLanguages: ${languages}\nPopulation: ${population}`;
+    // document.getElementById("countryInfo").innerText = ``;
+    // document.getElementById("countryInfo").innerText = ``;
+    // document.getElementById("countryInfo").innerText = ``;
+
+    // document.querySelector("body").appendChild()
 };
 
 
 
 getCountryByName("Bolivia")
 
+// document.querySelector("button").addEventListener("", getCountryByName);
