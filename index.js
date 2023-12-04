b67d861c029ec5c02faaeb013170492670378105
Kacper <kapelek82@outlook.com>
2023-12-04 00:11:04 +0000

    finished product however I didnt use query selector or container...

--- index.js
+++ index.js
@@ -2,34 +2,17 @@ const getCountryByName = async (countryName)  => {
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
     const jsonData = await response.json();
     const name = jsonData[0].name.common;
-    // document.querySelector("body").appendChild(name);
     const capital = jsonData[0].capital[0];
-    // document.querySelector("body").appendChild(capital);
     const languages = jsonData[0].languages;
-    // languages.array.forEach(element => {
+    const languagesArr = [];
         
     for (const key in languages) {
-        
-        console.log(`Found the value '${languages[key]}' at key '${key}'`);
+        languagesArr.push(languages[key]);
     }
-    // });
-    const population = jsonData[0].population;
-    // document.querySelector("body").appendChild(population);
-
-    console.log(languages);
-
     
-
-    document.getElementById("countryInfo").innerText = `Country Name: ${name}\nCapital: ${capital}\nLanguages: ${languages}\nPopulation: ${population}`;
-    // document.getElementById("countryInfo").innerText = ``;
-    // document.getElementById("countryInfo").innerText = ``;
-    // document.getElementById("countryInfo").innerText = ``;
-
-    // document.querySelector("body").appendChild()
+    const population = jsonData[0].population;
+    console.log(languages);
+    document.getElementById("countryInfo").innerText = `Country Name: ${name}\nCapital: ${capital}\nLanguages: ${languagesArr.join()}\nPopulation: ${population}`;
 };
 
-
-
-getCountryByName("Bolivia")
-
-// document.querySelector("button").addEventListener("", getCountryByName);
+getCountryByName("South Africa")
